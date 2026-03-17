a81e0aee749bef9767d470c68abe992d43ff1819
Muskan Patel <muskanpatel2003731@gmail.com>
2026-03-18 00:03:16 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDb = require("./config/database");
+const router = require('./routes');
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const corsOption = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
+
+connectDb();
+
+//middleware
+app.use(cors(corsOption));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api',router)
+
+app.listen(port, () => {
+console.log(`Server running on http://localhost:${port}`);
+});
